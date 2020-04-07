42d73fcd81bd8f0332e203e9c61208eccab57a9c
Shivang Patel <shivang.patel@spit.ac.in>
2020-04-07 17:58:55 +0530

    Adding code outline and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -9,13 +9,13 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.c> 
+ * @brief <Source file for Data Statistics functions performed on a Data Set>
  *
- * <Add Extended Description Here>
+ * <This file is used to do Data Statistics such as Mean, Median, Maximum, Minimum, Data Set Sorting>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Shivang Patel>
+ * @date <7th April, 2020>
  *
  */
 
@@ -36,8 +36,42 @@ void main() {
                                 7,  87, 250, 230,  99,   3, 100,  90};
 
   /* Other Variable Declarations Go Here */
+
+  unsigned char mean = 0;		//To store the mean value returned by 'find_mean' function
+  unsigned char median = 0;		//To store the median value returned by 'find_median' function
+  unsigned char maximum = 0;	//To store the maximum value returned by 'find_maximum' function
+  unsigned char minimum = 0;	//To store the minimum value returned by 'find_minimum' function
+ 
   /* Statistics and Printing Functions Go Here */
 
 }
 
 /* Add other Implementation File Code Here */
+	
+void print_statistics(unsigned char *arr_ptr, unsigned int arr_len, unsigned char mean, unsigned char median, unsigned char minimum, unsigned char maximum){
+
+}
+
+void print_array(unsigned char *arr_ptr, unsigned int arr_len){
+
+}
+
+unsigned char find_mean(unsigned char *arr_ptr, unsigned int arr_len){
+
+}
+
+unsigned char find_median(unsigned char *arr_ptr, unsigned int arr_len){
+
+}
+
+unsigned char find_maximum(unsigned char *arr_ptr, unsigned int arr_len){
+;
+}
+
+unsigned char find_minimum(unsigned char *arr_ptr, unsigned int arr_len){
+
+}
+
+unsigned char sort_array(unsigned char *arr_ptr, unsigned int arr_len){
+
+}
